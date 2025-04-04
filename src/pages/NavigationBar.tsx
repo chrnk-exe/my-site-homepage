ee663a144fe1c3ecff38b3c9a9de5e0819ad3312
chrnk-exe <ivan_kot2001@mail.ru>
2025-04-04 14:29:26 +0300

    some fixes

--- src/pages/NavigationBar.tsx
+++ src/pages/NavigationBar.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { FaLock } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
 import zxcgif from '../assets/zxc-cat.gif';
+import { Link } from 'react-router';
 
 const MotionBox = motion(Box);
 const MotionFlex = motion(Flex);
@@ -29,13 +30,13 @@ export default function NavBar() {
             gap={10}>
             <Image src={zxcgif} height={'60px'} />
             <MotionButton size="2xl" asChild>
-              <a href="/">Главная</a>
+              <Link to="/">Главная</Link>
             </MotionButton>
           </MotionFlex>
           <Stack direction={'row'} gap={6}>
             {navItems.map((item, index) => (
               <MotionButton key={index} size="2xl" asChild>
-                <a href={item.path}>{item.label}</a>
+                <Link to={item.path}>{item.label}</Link>
               </MotionButton>
             ))}
           </Stack>
